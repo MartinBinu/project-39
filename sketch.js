d42fe509da8e71a0e0f1e9fd38d03b036a836abf
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2021-03-28 20:43:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,167 +1,115 @@
-var bg, bgImage,gameOver,finished;
-var ig;
-var monkey, monkeyRunning,monkeyCrash;
-var BananaGroup,bannanaImage;
-var ObstacleGroup,RockGroup,obstacleImage2;
-var score = 0;
-var bananaCount = 0;
-var button;
-var chance;
+  var Engine = Matter.Engine,
+    World = Matter.World,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies;
 
-function preload(){
+  var gameState = "start";
+  var particle;
+  var particle2;
 
-  bgImage = loadImage("pic/jungle2.jpg");
+  var playButton;
 
-  gameOver = loadImage("pic/game Over.jpg")
+  var divisions = [];
+  var particles = [];
+  var plinkos = [];
 
-  monkeyRunning = loadAnimation("pic/Monkey_01.png","pic/Monkey_02.png","pic/Monkey_03.png","pic/Monkey_04.png","pic/Monkey_05.png","pic/Monkey_06.png","pic/Monkey_07.png","pic/Monkey_08.png","pic/Monkey_09.png","pic/Monkey_10.png");
+  var divisionHeight = 300;
+  var stage = 0;
+  var score = 0;
+  var turn = 0;
 
-  monkeyCrash = loadAnimation("pic/Monkey_01.png");  
+  function setup() {
 
-  bananaImage = loadImage("pic/Banana.png");
+    createCanvas(800, 800);
 
-  obstacleImage = loadImage("pic/stone.png");
+    engine = Engine.create();
+    world = engine.world;
 
-}
+    ground = new Ground(width/2,height,width,20);
 
-function setup(){ 
+    playButton = createButton('Play');
 
-   createCanvas(displayWidth,displayHeight - 100);
+    for(var k = 0; k <=width; k = k + 80) {
+      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+    }
 
-   chance = 2;
 
-   bg = createSprite(displayWidth/2,displayHeight/2)
-   bg.addImage("bg",bgImage);
-   bg.width = displayWidth + 100;
-   bg.scale = 1.59;
+    for(var j = 75; j <=width; j=j+50){
+      plinkos.push(new Plinko(j,75));
+    }
 
-   button = createButton("Retry")
-   button.position(displayWidth/2,displayHeight/2 + 20);
+    for(var j = 50; j <=width-10; j=j+50){
+      plinkos.push(new Plinko(j,175));
+    }
 
-   monkey = createSprite(80,400);
-   monkey.addAnimation("monkeyRunning",monkeyRunning);
-   monkey.scale = 0.1;
+    for(var j = 75; j <=width; j=j+50) {
+      plinkos.push(new Plinko(j,275));
+    }
 
-   ig = createSprite(200,650,400,30);
-   ig.visible = false;
+    for(var j = 50; j <=width-10; j=j+50){
+      plinkos.push(new Plinko(j,375));
+    }
 
-   BananaGroup = new Group();
-
-   ObstacleGroup = new Group();
-
-   RockGroup = new Group();
-
-}
-
-function draw() {
-
-   fill("white");
-
-   background("green");
-
-   edges = createEdgeSprites(); 
-
-   monkey.collide(ig);
-   monkey.collide(edges[2]);
-
-   camera.position.x = displayWidth/2;
-   camera.position.y = displayHeight/2 - 50;
-
-   if(chance > 0){
-
-      bg.velocityX = -(3 + 50*score/100);
-
-      monkey.scale = 0.1 + bananaCount/100;
-      monkey.velocityY = monkey.velocityY + 0.6;
-   
-      button.hide();
-
-      if(keyDown("space")){
-         monkey.velocityY = -5;
-      }
-   
-      if(bg.x < 612){
-        bg.x = displayWidth - 700;
-      }
-   
-      for(var i = 0;i<BananaGroup.length;i++){
-         if(monkey.isTouching(BananaGroup.get(i))){
-            BananaGroup.get(i).destroy();
-            score = score + 1;
-            bananaCount = bananaCount + 1;
-         }
-      }
-
-      for(var i = 0;i<ObstacleGroup.length;i++){
-         if(monkey.isTouching(ObstacleGroup.get(i))){
-            ObstacleGroup.get(i).destroy();
-            bananaCount = 0;
-            monkey.y = 400;
-            chance = chance - 1;        
-         }
-      }
-   
-
-      console.log(BananaGroup.length);
-
-      spawnObstacles();
-
-   }
-
-if(chance === 0){
-
-   bg.velocityX = 0;
-   button.show();
-
-   button.mousePressed(function(){
-      chance = 2;
-   })
-
-}
-
-   drawSprites();
-
-   textSize(15);
-   text("Banana Count: " + bananaCount,10,50)
-   text("chances: " + chance,10,70);
-
-   if(chance === 0){
-   textSize(20);
-   text("Game Over",displayWidth/2 - 10,displayHeight/2);
-   }
-
-   spawnBananas();
-
-}
-
-function spawnBananas() {
+    playButton.position(width/2 + 18,height/2 + 13);
+  }
   
-    
+  function draw() {
+    background("black");
+    textSize(20);
+    Engine.update(engine);
 
-   if(frameCount % 120 === 0){ 
-    var banana = createSprite(displayWidth,300);
-    banana.addImage("banana_1",bananaImage);
-    banana.scale = 0.03;
-    banana.velocityX = -(3 + 50*score/100)
-    banana.y = Math.round(random(100,displayHeight - 200));
-    banana.lifetime = -1;
-    BananaGroup.add(banana); 
- 
-   }
+    for (var i = 0; i < plinkos.length; i++) {  
+      plinkos[i].display();
+    }
+
+    for (var k = 0; k < divisions.length; k++) {
+      divisions[k].display();
+    }
+
+
+  if(stage === 0){
+    for (var j = 0; j < particles.length; j++) {
+      particles[j].display();
+    }
+
+    if(gameState === "start"){
+      if(frameCount%10===0){
+        particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+      }
+      
+      playButton.mousePressed(()=>{
+        gameState = "play";
+        stage = 1;
+      })
+    }
 
   }
-    
-  function spawnObstacles() {
-    
-   if(frameCount % 100 === 0){ 
-     var obstacles = createSprite(displayWidth,350); 
-     obstacles.addImage("stone",obstacleImage);
-     obstacles.scale = 0.1;
-     obstacles.velocityX = -(3 + 50*score/100);
-     obstacles.y = Math.round(random(100,displayHeight - 200));
-     obstacles.lifetime = -1;
-     ObstacleGroup.add(obstacles);
 
-   }
+  if(gameState === "start"){
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    textSize(100);
+    text("Plinko",width/2 - 110,height/2)
 
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    textSize(20);
+    text(" press ",width/2 - 50,height/2 + 20)
   }
+
+  if(gameState === "play"){
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    textSize(20);
+    text("Score: "+ score,width - 100,20)
+
+    playButton.hide();
+
+    particle.display();
+
+    mousePressed();
+  }
+}
+
+function mousePressed(){
+   if(gameState === "play"){
+    particle = new Particle2(mouseX,40,10);
+   }
+}
