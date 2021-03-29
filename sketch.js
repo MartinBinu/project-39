170a9fb4a5e0597dc60ccd54df856b610758f69d
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2021-03-29 17:34:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,115 +1,54 @@
-  var Engine = Matter.Engine,
-    World = Matter.World,
-    Events = Matter.Events,
-    Bodies = Matter.Bodies;
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
 
-  var gameState = "start";
-  var particle;
-  var particle2;
-
-  var playButton;
-
-  var divisions = [];
-  var particles = [];
-  var plinkos = [];
-
-  var divisionHeight = 300;
-  var stage = 0;
-  var score = 0;
-  var turn = 0;
-
-  function setup() {
-
-    createCanvas(800, 800);
-
-    engine = Engine.create();
-    world = engine.world;
-
-    ground = new Ground(width/2,height,width,20);
-
-    playButton = createButton('Play');
-
-    for(var k = 0; k <=width; k = k + 80) {
-      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-    }
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
 
 
-    for(var j = 75; j <=width; j=j+50){
-      plinkos.push(new Plinko(j,75));
-    }
-
-    for(var j = 50; j <=width-10; j=j+50){
-      plinkos.push(new Plinko(j,175));
-    }
-
-    for(var j = 75; j <=width; j=j+50) {
-      plinkos.push(new Plinko(j,275));
-    }
-
-    for(var j = 50; j <=width-10; j=j+50){
-      plinkos.push(new Plinko(j,375));
-    }
-
-    playButton.position(width/2 + 18,height/2 + 13);
-  }
-  
-  function draw() {
-    background("black");
-    textSize(20);
-    Engine.update(engine);
-
-    for (var i = 0; i < plinkos.length; i++) {  
-      plinkos[i].display();
-    }
-
-    for (var k = 0; k < divisions.length; k++) {
-      divisions[k].display();
-    }
-
-
-  if(stage === 0){
-    for (var j = 0; j < particles.length; j++) {
-      particles[j].display();
-    }
-
-    if(gameState === "start"){
-      if(frameCount%10===0){
-        particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
-      }
-      
-      playButton.mousePressed(()=>{
-        gameState = "play";
-        stage = 1;
-      })
-    }
-
-  }
-
-  if(gameState === "start"){
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    textSize(100);
-    text("Plinko",width/2 - 110,height/2)
-
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    textSize(20);
-    text(" press ",width/2 - 50,height/2 + 20)
-  }
-
-  if(gameState === "play"){
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    textSize(20);
-    text("Score: "+ score,width - 100,20)
-
-    playButton.hide();
-
-    particle.display();
-
-    mousePressed();
-  }
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
 }
 
-function mousePressed(){
-   if(gameState === "play"){
-    particle = new Particle2(mouseX,40,10);
-   }
+function setup() {
+
+  createCanvas(1000, 600);
+
+  database = firebase.database();
+
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  
+  //background(back_img);
+  
+  if(playerCount === 2) {
+    game.update(1);
+  }
+  if(gameState === 1) {
+    clear(); 
+    game.play();
+  }
+  if(gameState === 2) {   
+    game.end();
+  }
+
 }
